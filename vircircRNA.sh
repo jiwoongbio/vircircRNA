8ee5e3ac66247ea88501f4e9400be00b13311b18
Jiwoong Kim <jkim23@bunny.swmed.edu>
2018-10-06 22:36:54 -0500

    first commit

--- vircircRNA.sh
+++ vircircRNA.sh
@@ -9,21 +9,20 @@ fastqFiles=${@:4}
 
 if [ -z "$fastqFiles" ]; then
 	echo 'Usage: ./vircircRNA.sh <chromosome.fasta> <gene.gff> <output.prefix> <input.1.fastq> [input.2.fastq]' 1>&2
-	echo 1>&2
 	exit 1
 fi
 
-perl $directory/vircircRNA_chromosome.pl $fastaFile > $outputPrefix.vircircRNA_chromosome.fasta
+perl $directory/vircircRNA_chromosome.pl $fastaFile > $outputPrefix.vircircRNA_chromosome.fasta || exit 1
 
-bwa index $outputPrefix.vircircRNA_chromosome.fasta
+bwa index $outputPrefix.vircircRNA_chromosome.fasta || exit 1
 if [ -x "$(command -v samtools)" ]; then
-	bwa mem -T 19 -Y $outputPrefix.vircircRNA_chromosome.fasta $fastqFiles | samtools view -S -h -F 4 - > $outputPrefix.vircircRNA_chromosome.sam
+	bwa mem -T 19 -Y $outputPrefix.vircircRNA_chromosome.fasta $fastqFiles | samtools view -S -h -F 4 - > $outputPrefix.vircircRNA_chromosome.sam || exit 1
 else
-	bwa mem -T 19 -Y $outputPrefix.vircircRNA_chromosome.fasta $fastqFiles > $outputPrefix.vircircRNA_chromosome.sam
+	bwa mem -T 19 -Y $outputPrefix.vircircRNA_chromosome.fasta $fastqFiles > $outputPrefix.vircircRNA_chromosome.sam || exit 1
 fi
 
-perl $directory/vircircRNA_junction.pl -g $gffFile -A $outputPrefix.vircircRNA_junction.alignment.html $outputPrefix.vircircRNA_chromosome.sam $outputPrefix.vircircRNA_chromosome.fasta > $outputPrefix.vircircRNA_junction.txt
+perl $directory/vircircRNA_junction.pl -g $gffFile -A $outputPrefix.vircircRNA_junction.alignment.html $outputPrefix.vircircRNA_chromosome.sam $outputPrefix.vircircRNA_chromosome.fasta > $outputPrefix.vircircRNA_junction.txt || exit 1
 
-if perl -MGD -MGD::Text::Align -e '' 2> /dev/null; then
-	perl $directory/vircircRNA_diagram.pl -g $gffFile $outputPrefix.vircircRNA_junction.txt $outputPrefix.vircircRNA_chromosome.fasta > $outputPrefix.vircircRNA_diagram.png
+if perl -MGD -MGD::Text::Align -e ''; then
+	perl $directory/vircircRNA_diagram.pl -g $gffFile $outputPrefix.vircircRNA_junction.txt $outputPrefix.vircircRNA_chromosome.fasta > $outputPrefix.vircircRNA_diagram.png || exit 1
 fi
